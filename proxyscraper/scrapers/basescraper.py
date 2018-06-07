3df82b098600f59d4d706107293215f12118b1b5
Ovidijus Okinskas <ovidijus1@live.com>
2018-06-07 21:56:21 +0100

    Restructuring to account for selenium usage which makes the scraping slower and more difficult. Added drivers, page manipulation and scraping of data (just prints at the moment).

--- proxyscraper/scrapers/basescraper.py
+++ proxyscraper/scrapers/basescraper.py
@@ -1,45 +1,30 @@
-from bs4 import BeautifulSoup
-from abc import ABC, abstractmethod
-from typing import Dict
+from time import sleep
+from abc import ABC
+
+from selenium import webdriver
 
 
 class BaseScraper(ABC):
 
-    def __init__(self, name: str, url: str, data: Dict, options: Dict):
+    def __init__(self, name: str, url: str):
         """Create a scraper with a name, corresponding url endpoint and options dict for mapping methods.
 
         :param name: (str): Name of scraper.
         :param url (str): Endpoint to interact with.
-        :param data (Dict): Form data used in POST request.
-        :param options (Dict): Dictionary containing mappings of CSS-selectors and corresponding function to be executed
         """
         self._name = name or 'Base'
         self._url = url or 'BaseUrl'
-        self._data = data or {}
-        self._options = options or {'': lambda x: x}
+        self._driver = None
 
-    @abstractmethod
-    def scrape(self, *args, **kwds):
-        """Scrape data from endpoint via given parameters.
+    def start_driver(self):
+        print('starting driver...')
+        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+        sleep(4)
 
-        :param args: Variable length argument list.
-        :param kwds: Arbitrary keyword arguments.
-        :return: List[PlanningResult]: List containing PlanningResults scraped and parsed.
-        """
-        pass
-
-    def parse_results(self, soup: BeautifulSoup):
-        """Parse the results of a HTML page directly in accordance with CSS-selectors and corresponding methods.
-        :param soup: (BeautifulSoup): HTML page.
-        :return: List[PlanningResult]: List containing PlanningResult objects created from HTML page.
-        """
-        results = []
-        for key in self.options.keys():
-            try:
-                results += self.options.get(key)(soup.select(key))
-            except TypeError:
-                continue
-        return results
+    def close_driver(self):
+        print('closing driver...')
+        self.driver.quit()
+        print('closed!')
 
     @property
     def name(self):
@@ -58,17 +43,9 @@ class BaseScraper(ABC):
         self._url = url
 
     @property
-    def data(self):
-        return self._data
+    def driver(self):
+        return self._driver
 
-    @data.setter
-    def data(self, data):
-        self._data = data
-
-    @property
-    def options(self):
-        return self._options
-
-    @options.setter
-    def options(self, options):
-        self._options = options
+    @driver.setter
+    def driver(self, driver):
+        self._driver = driver
--- proxyscraper/scrapers/freeproxylist.py
+++ proxyscraper/scrapers/freeproxylist.py
@@ -1,17 +1,39 @@
+from random import randint
+
 from proxyscraper.scrapers.basescraper import BaseScraper
+from time import sleep
 
 
 class FreeProxyListScraper(BaseScraper):
 
     def __init__(self):
-        """
-        Note: May need to rethink BaseScraper as parsing may become more difficult with current setup.
-            ie. more page manipulation may be involved.
-        """
         name = 'FreeProxyList'
         url = 'https://free-proxy-list.net/'
-        options = {}
-        super().__init__(name, url, options)
+        super().__init__(name, url)
+        self._driver = None
+
+    def get_page(self):
+        print('getting page...')
+        self.driver.get(self.url)
+        sleep(randint(3, 4))
+
+    def manip_table(self):
+        print('manipulating page...')
+        element = self.driver.find_element_by_css_selector('#proxylisttable_length > label > select')
+        last = element.find_elements_by_tag_name('option')[-1]
+        self.driver.execute_script('arguments[0].setAttribute("value", 300)', last)
+        last.click()
 
     def scrape(self):
-        pass
+        print('scraping page...')
+        element = self.driver.find_element_by_css_selector('#proxylisttable > tbody')
+        all_rows = [i.text for i in element.find_elements_by_xpath('./tr')]
+        [print(x) for x in all_rows]
+
+    @property
+    def driver(self):
+        return self._driver
+
+    @driver.setter
+    def driver(self, driver):
+        self._driver = driver
